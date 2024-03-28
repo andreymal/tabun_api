5f30544d1a7e3cf03a3f82da745b9c0b92fbf9fc
andreymal <andriyano-31@mail.ru>
2024-03-28 16:05:46 +0300

    Bump year and version

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,7 +26,7 @@ import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 
 project = 'tabun_api'
-copyright = '2023, andreymal'
+copyright = '2024, andreymal'
 author = 'andreymal'
 
 # The short X.Y version
